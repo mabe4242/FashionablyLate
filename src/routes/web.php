5d5392c5d39eeca3287269d21365e717da9d9eee
mabe4242 <kouiy42@gmail.com>
2025-07-13 02:13:17 +0900

    管理画面の各メソッドをミドルウェアで制御

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,14 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/admin', [ContactController::class, 'index']);
-});
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+// 公開ページ
 Route::get('/', [ContactController::class, 'create'])->name('contacts.create');
 Route::post('/confirm', [ContactController::class, 'confirm']);
 Route::get('/contacts/back', [ContactController::class, 'back'])->name('contacts.back');
 Route::post('/contacts', [ContactController::class, 'store']);
-Route::delete('/delete', [ContactController::class, 'destroy']);
-Route::get('/search', [ContactController::class, 'search']);
-Route::get('/export', [ContactController::class, 'export'])->name('contacts.export');
+
+// 認証が必要な管理ページ
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [ContactController::class, 'index']);
+    Route::delete('/delete', [ContactController::class, 'destroy']);
+    Route::get('/search', [ContactController::class, 'search']);
+    Route::get('/export', [ContactController::class, 'export'])->name('contacts.export');
+});
+
+// ログアウト後のリダイレクト
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
