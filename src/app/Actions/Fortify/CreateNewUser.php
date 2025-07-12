5ff10ac9a79b987ad9868ee5d2a743100b7fb957
mabe4242 <kouiy42@gmail.com>
2025-07-13 00:21:34 +0900

    userテーブルのカラムを修正

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -5,18 +5,11 @@ namespace App\Actions\Fortify;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
-
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @param  array<string, string>  $input
-     */
     public function create(array $input): User
     {
         Validator::make($input, [
@@ -26,9 +19,13 @@ class CreateNewUser implements CreatesNewUsers
                 'string',
                 'email',
                 'max:255',
-                Rule::unique(User::class),
+                'unique:users',
             ],
-            'password' => $this->passwordRules(),
+            'password' => [
+                'required',
+                'string',
+                Password::min(8),
+            ],
         ])->validate();
 
         return User::create([
